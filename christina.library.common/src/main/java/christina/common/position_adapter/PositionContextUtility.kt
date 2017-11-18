48b7e203150c8d0feec11a94c5c6789db12f6ab8
Christina <christina.prj.dev@gmail.com>
2017-11-18 13:19:36 +0300

    Added Indexer

--- christina.library.common/src/main/java/christina/common/position_adapter/PositionContextUtility.kt
+++ christina.library.common/src/main/java/christina/common/position_adapter/PositionContextUtility.kt
@@ -1,10 +1,10 @@
 package christina.common.position_adapter
 
-fun positionContext(getPositionStart: () -> Int, getPositionEnd: () -> Int): PositionContext =
+fun positionContext(positionStart: () -> Int, positionEnd: () -> Int): PositionContext =
     object : PositionContext {
-        override fun getPositionStart(): Int = getPositionStart()
+        override fun getPositionStart(): Int = positionStart()
 
-        override fun getPositionEnd(): Int = getPositionEnd()
+        override fun getPositionEnd(): Int = positionEnd()
     }
 
 fun positionContext(positionStart: Int, positionEnd: Int): PositionContext =
