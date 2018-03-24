f45c041c9e23ec6e9188e7e7ca613e8601279aa8
Christina <christina.prj.dev@gmail.com>
2018-03-24 12:18:07 +0300

    Update versions.

--- christina.library.common/src/main/kotlin/christina/common/rx/ObserverExtensions.kt
+++ christina.library.common/src/main/kotlin/christina/common/rx/ObserverExtensions.kt
@@ -1,9 +1,14 @@
 package christina.common.rx
 
+import io.reactivex.Flowable
 import io.reactivex.Observable
 import io.reactivex.ObservableSource
 import io.reactivex.Observer
 
 fun Observer<Unit>.onNext() = onNext(Unit)
+
+fun <T> Observable<T>.onErrorComplete() = onErrorResumeNext(Observable.empty())
+
+fun <T> Flowable<T>.onErrorComplete() = onErrorResumeNext(Flowable.empty())
 
 fun <T> ObservableSource<out ObservableSource<out T>>.switchOnNext() = Observable.switchOnNext(this)
